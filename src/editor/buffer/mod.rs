5472e7fa7e5b704124ad9a7f9a11d620e93f5904
Huy-DNA <huydo862003@gmail.com>
2025-08-17 23:20:35 +0700

    refactor: better namings for methods of buffer

--- src/editor/buffer/mod.rs
+++ src/editor/buffer/mod.rs
@@ -1,6 +1,7 @@
 use super::utility::{Direction, Location};
 use ropey::{Rope, iter::Chars};
 use std::{
+    cmp::min,
     fs::File,
     io::{BufReader, Error},
     path::PathBuf,
@@ -9,14 +10,14 @@ use std::{
 pub struct Buffer {
     path: Option<PathBuf>,
     text: Rope,
-    cursor: Location,
+    raw_cursor_location: Location,
 }
 
 impl Buffer {
     pub fn new(path: Option<PathBuf>) -> Result<Buffer, Error> {
         Ok(Buffer {
             path: path.clone(),
-            cursor: Location::default(),
+            raw_cursor_location: Location::default(),
             text: if path.is_none() {
                 Rope::new()
             } else {
@@ -28,7 +29,7 @@ impl Buffer {
     }
 
     pub fn get_cursor(&self) -> Location {
-        self.cursor
+        self.get_effective_cursor_location()
     }
 
     pub fn get_path(&self) -> Option<&str> {
@@ -42,34 +43,42 @@ impl Buffer {
     pub fn move_cursor(&mut self, direction: Direction) {
         match direction {
             Direction::Up => {
-                if self.cursor.y == 0 {
+                if self.raw_cursor_location.y == 0 {
                     return;
                 }
-                self.cursor.y -= 1;
-                let line_length = self.get_line_length(self.cursor.y).unwrap_or(0);
-                if self.cursor.x >= line_length {
-                    self.cursor.x = line_length;
-                }
+                self.raw_cursor_location.y -= 1;
             }
             Direction::Down => {
                 let line_count = self.get_line_count();
-                if self.cursor.y + 1 >= line_count {
+                if self.raw_cursor_location.y + 1 >= line_count {
                     return;
                 }
-                self.cursor.y += 1;
-                let line_length = self.get_line_length(self.cursor.y).unwrap_or(0);
-                if self.cursor.x >= line_length {
-                    self.cursor.x = line_length;
-                }
+                self.raw_cursor_location.y += 1;
             }
-            Direction::Left => self.cursor.x = self.cursor.x.saturating_sub(1),
+            Direction::Left => self.raw_cursor_location.x = self.clamp_cursor_x().saturating_sub(1),
             Direction::Right => {
-                let line_length = self.get_line_length(self.cursor.y).unwrap_or(0);
-                if self.cursor.x < line_length {
-                    self.cursor.x += 1;
+                let line_length = self
+                    .get_line_length(self.raw_cursor_location.y)
+                    .unwrap_or(0);
+                if self.raw_cursor_location.x < line_length {
+                    self.raw_cursor_location.x += 1;
                 }
             }
         }
+    }
+
+    fn get_effective_cursor_location(&self) -> Location {
+        Location {
+            x: self.clamp_cursor_x(),
+            y: self.raw_cursor_location.y,
+        }
+    }
+
+    fn clamp_cursor_x(&self) -> usize {
+        let line_length = self
+            .get_line_length(self.raw_cursor_location.y)
+            .unwrap_or(0);
+        min(self.raw_cursor_location.x, line_length)
     }
 
     pub fn get_line(&self, line: usize) -> Option<Chars> {
