f1c5cd6ba245f9952e2236aa815b50f14d36eb8b
Huy-DNA <huydo862003@gmail.com>
2025-08-27 16:30:08 +0700

    fix: remove powerline component

--- src/editor/app/mod.rs
+++ src/editor/app/mod.rs
@@ -1,17 +1,13 @@
+use super::{component::textarea::Textarea, drawing_surface::DrawingSurface};
+use crossterm::event::KeyEvent;
 use std::{
     fs::File,
     io::{Error, Read},
     path::PathBuf,
 };
-use crossterm::event::KeyEvent;
-use super::{
-    component::{powerline::Powerline, textarea::Textarea},
-    drawing_surface::DrawingSurface,
-};
 
 pub struct App {
     content_area: Textarea,
-    powerline: Powerline,
 }
 
 impl App {
@@ -19,18 +15,14 @@ impl App {
         if path.is_none() {
             Ok(App {
                 content_area: Textarea::new(""),
-                powerline: Powerline::new("[No Name] - 0 line(s)"),
             })
         } else {
             let file_path = path.unwrap().to_string_lossy().into_owned();
             let mut file = File::open(&file_path)?;
             let mut content = String::new();
             file.read_to_string(&mut content)?;
-            let content_area = Textarea::new(&content);
-            let lines = content_area.get_content().get_line_count();
             Ok(App {
                 content_area: Textarea::new(&content),
-                powerline: Powerline::new(&format!("{file_path} - {lines} line(s)")),
             })
         }
     }
